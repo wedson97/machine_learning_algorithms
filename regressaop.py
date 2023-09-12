c04cb6ffb1ef9bbcad0e02750e90f49b06f440e5
Wedson <wedsonsilva025@gmail.com>
2023-09-11 22:57:33 -0300

    Finalização

--- regressaop.py
+++ regressaop.py
@@ -20,5 +20,32 @@ for i, pair in enumerate(column_pairs):
     x_col, y_col = pair
     sns.scatterplot(x=x_col,y=y_col,data=base,ax=axes[i])
     axes[i].set_title(f'{x_col} vs {y_col}')
-plt.tight_layout()
+#aic 156.6  bic 162.5
+#modelo = sm.ols(formula='mpg ~ wt + disp + hp',data=base)
+
+#aic 165.1  bic 169.5
+#modelo = sm.ols(formula='mpg ~  disp + cyl',data=base)
+
+modelo = sm.ols(formula='mpg ~ drat + vs',data=base)
+modelo = modelo.fit()
+
+print(modelo.summary())
+
+residuos = modelo.resid
+plt.hist(residuos, bins=20)
+plt.xlabel("Residuos")
+plt.ylabel("Frequencia")
+plt.title("Histograma de Residous")
+
+stats.probplot(residuos,dist='norm',plot=plt)
+plt.title("Q-Q plot de Residous")
+
+#h0 - dados estão normalmente distribuidos
+# p <= 0.05 rejeito a hipostese nula, (não estão normalmente distribuidos)
+# p > 0.05 não é possivel rejeitar o h0
+stat, pval = stats.shapiro(residuos)
+print(f'Shapiro-Wilk statistics: {stat:.3f}, p-value: {pval:.3f}')
+
 plt.show()
+
+plt.tight_layout()
